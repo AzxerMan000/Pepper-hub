9b029b71ac2b78b5ca824b090576f25b2e181f1b
Azxerman <bangjoof12@gmail.com>
2025-07-24 17:38:01 +0700

    Create Rejoin.lua

---
+++ Rejoin.lua
@@ -0,0 +1,9 @@
+local TeleportService = game:GetService("TeleportService")
+local Players = game:GetService("Players")
+
+local function rejoin()
+    local player = Players.LocalPlayer
+    TeleportService:Teleport(game.PlaceId, player)
+end)
+  
+  rejoin()
